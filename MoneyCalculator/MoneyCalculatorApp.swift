12421615c0a588503760c3765440364c2704111c
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-09-30 10:11:23 +0900

    Initial Commit

---
+++ MoneyCalculator/MoneyCalculatorApp.swift
@@ -0,0 +1,32 @@
+//
+//  MoneyCalculatorApp.swift
+//  MoneyCalculator
+//
+//  Created by Shumpei Horiuchi on 2024/09/30.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MoneyCalculatorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
